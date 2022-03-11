edc5b76cfe603a41f6f3f9c3e4ae1aba29825953
lazuardi100 <lazuardi.akhmad@gmail.com>
2022-03-11 16:04:20 +0700

    mutation work

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -4,8 +4,5 @@ module Mutations
     field_class Types::BaseField
     input_object_class Types::BaseInputObject
     object_class Types::BaseObject
-
-    field :search_customer, mutation: Mutations::SearchCustomer
-    # field :delete_customer, mutation: Mutations::DeleteCustomer
   end
 end
